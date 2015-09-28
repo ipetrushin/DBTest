47a3169465e20fc35b04e48976f4f9ff2717f6f5
ipetrushin <ivan.kiel@gmail.com>
2015-09-29 00:49:12 +0800

    update 2

--- app/src/main/java/com/example/gizmo/dbtest/MainActivity.java
+++ app/src/main/java/com/example/gizmo/dbtest/MainActivity.java
@@ -7,9 +7,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -18,14 +21,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Button tap;
+    private static final int CM_DELETE_ID = 1;
+
+    // there are two modes for tapbutton
+    private static final int MODE_ADD = 0;
+    private static final int MODE_EDIT = 1;
+    int mode = MODE_ADD;
+
+    // "global" used vars
+    Button tap;
     SQLiteDatabase mDB;
     MyDBHelper DB;
     EditText note;
-    TextView recordscount;
+    TextView recordscount, displayid;
     ListView lv;
     SimpleCursorAdapter scAdapter;
     Cursor c;
+
     // arrays for binding DB and ListView
     String[] headers = {"_id", "timestamp", "note"};
     int[] fields;
@@ -38,8 +50,10 @@ public class MainActivity extends AppCompatActivity {
         // binding controls
         note = (EditText) findViewById(R.id.noteText);
         recordscount = (TextView) findViewById(R.id.recordscount);
+        displayid= (TextView) findViewById(R.id.displayid);
         lv = (ListView) findViewById(R.id.listView);
         fields = new int[]  {R.id.listid, R.id.listtimestamp, R.id.listnote,};
+        tap = (Button) findViewById(R.id.tapButton);
 
         DB = new MyDBHelper(this);
         mDB = DB.getWritableDatabase();
@@ -50,16 +64,45 @@ public class MainActivity extends AppCompatActivity {
         // binding adaper with ListView
         scAdapter = new SimpleCursorAdapter(this, R.layout.item, c, headers, fields);
         lv.setAdapter(scAdapter);
+        registerForContextMenu(lv);
+
+        // binding click listener for items of listview
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            String notetext;
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                c.moveToPosition( position);
+                notetext = c.getString(2);
+                Log.d("DeBUG", "itemClick: position = " + position + ", id = "
+                        + id + " value = " + notetext);
+                displayid.setText(Long.toString(id));
+                note.setText(notetext);
+                mode = MODE_EDIT;
+                tap.setText("EDIT NOTE");
+            }
+        });
 
     }
 
     public void tapClick(View v)
     {
-        ContentValues cv = new ContentValues(1);
-        cv.put("note",note.getText().toString());
-        mDB.insert("notes", null, cv);
 
-        c.requery();
+        if (mode == MODE_ADD) {
+            ContentValues cv = new ContentValues(1);
+            cv.put("note", note.getText().toString());
+            mDB.insert("notes", null, cv);
+        }
+        if (mode == MODE_EDIT) {
+            int id = Integer.parseInt(displayid.getText().toString());
+            mDB.execSQL("UPDATE notes SET note = '" + note.getText().toString() + "' WHERE _id = " + id);
+
+            mode = MODE_ADD;
+            displayid.setText("");
+            note.setText("");
+            tap.setText("ADD NOTE");
+        }
+        c.requery(); // refresh listview
         recordscount.setText("Records in table: " + Integer.toString(c.getCount()));
     }
 
@@ -67,7 +110,12 @@ public class MainActivity extends AppCompatActivity {
     {
         String[] where = {};
         mDB.delete("notes", "", where);
-        c.requery();
+        c.requery(); // refresh listview
+
+        mode = MODE_ADD;
+        displayid.setText("");
+        note.setText("");
+        tap.setText("ADD NOTE");
     }
 
     @Override
@@ -77,14 +125,30 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_DELETE_ID, 0, "Delete");
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == CM_DELETE_ID) {
+
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+            mDB.delete("notes", "_id = " + acmi.id, null);
+            c.requery(); // refresh listview
+
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
             return true;
         }
@@ -102,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                 " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP, "
                 + COLUMN_NOTE + " TEXT NOT NULL);";
-        private SQLiteDatabase mydb;
+
 
         public MyDBHelper(Context ctx) {
             super(ctx, DB_NAME, null, DB_VERSION);
@@ -110,11 +174,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(DB_CREATE_TABLE);
-            mydb = db;
-            ContentValues cv = new ContentValues(1);
+
+            //ContentValues cv = new ContentValues(1);
         }
         public void reCreate(SQLiteDatabase db)
         {
+            // doesn't work. leave it here for future
             db.execSQL("DROP DATABASE "+DB_TABLE);
             db.execSQL(DB_CREATE_TABLE);
         }
